309887204d99d3553a3d9945aa413c8ad2db47be
sereda92 <assakay92@gmail.com>
2023-12-14 15:41:03 +0200

    new file

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+// -------------------SPEED SCROLL----------------------------
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors){
+	anchor.addEventListener("click", function(event){
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+    })
+})
+}
+
+// --------------------FIXED HEADER-------------------------
+const header = document.querySelector('.header_container');
+const first = document.querySelector('.block-contakt');
+const imgLogo = document.querySelector('.logo-img');
+const btnArrow = document.querySelector('.btn-arrow');
+const arrow = document.querySelector('.arrow');
+
+const headerHeight = header.offsetHeight;
+const firstHeight = first.offsetHeight;
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+
+	if (scrollDistance >= firstHeight + headerHeight) {
+		header.classList.add('header--fixed');
+        imgLogo.classList.add('logo-img-scroll')
+        arrow.classList.add('arrow-fixed')
+	} else {
+		header.classList.remove('header--fixed');
+		first.style.marginTop = null;
+        imgLogo.classList.remove('logo-img-scroll')
+        arrow.classList.add('arrow')
+	}
+
+
+	if (scrollDistance === 0) {
+		header.classList.remove('header--fixed');
+        imgLogo.classList.remove('logo-img-scroll')
+        arrow.classList.remove('arrow-fixed')
+		first.style.marginTop = null;
+	}
+
+	lastScrollTop = scrollDistance;
+});
+
+
